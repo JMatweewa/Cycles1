330b8411d09dc64062b668963f339647264cf0bc
JMatweewa <93136237+JMatweewa@users.noreply.github.com>
2021-12-14 11:02:01 +0700

    added tests to check the boundary values

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -10,7 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"counter,100,500, 13"})
+    @CsvSource(value = {"checking the boundary values for the input data 10 & 99,100,9801, 90",
+            "checking the boundary values for the input data 11 & 98, 121, 9604, 88",
+            "checking the boundary values for the input data 9 & 100, 81, 10000, 90",
+            "checking the boundary values for the input data 50 & 60, 2500, 3600, 11",
+            "checking the boundary values for the input data 0 & 0, 0, 0, 0"})
     void countTheNumberOfSquares(String name, int min, int max, int expected) {
         SQRService service = new SQRService();
 
